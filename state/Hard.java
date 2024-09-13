fe12ad7663877e9ff491e75d0fea3ddaeaaa1f69
sspadaro <spadaros@email.sc.edu>
2024-09-13 19:39:40 -0400

    hard class

--- state/Hard.java
+++ state/Hard.java
@@ -1,21 +1,36 @@
 package state;
 
+import java.util.Random;
+
 public class Hard {
     private ArithmeticGame game;
 
     public Hard(ArithmeticGame game){
-
+        this.game = game;
     }
     public int getNum(){
-
+        Random rand = new Random();
+        int num = rand.nextInt(51);
+        return num;
     }
     public Operation getOperation(){
-
+        Random rand = new Random();
+        int num = rand.nextInt(4);
+        if(num == 0){
+            return Operation.PLUS;
+        }else if(num == 1){
+            return Operation.MINUS;
+        }else if(num == 2){
+            return Operation.MULTIPLY;
+        }else{
+            return Operation.DIVIDE;
+        }
     }
     public void increaseDifficulty(){
-
+        System.out.println("Good job");
     }
     public void decreaseDifficulty(){
-        
+        game.setState(game.getMediumState());
+        System.out.println("Level decreased to: Medium");
     }
 }
