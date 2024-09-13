0e521654b8e348d9fad902419a8b08dfa2b4dcb4
sspadaro <spadaros@email.sc.edu>
2024-09-13 19:12:06 -0400

    Added methods

---
+++ state/Question.java
@@ -0,0 +1,19 @@
+package state;
+
+public class Question {
+    private String question;
+    private int answer;
+
+    public Question(int num1, int num2, Operation operation){
+
+    }
+    public String toString(){
+
+    }
+    public int getAnswer(){
+
+    }
+    public boolean isCorrect(int answer){
+        
+    }
+}
