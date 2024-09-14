87c6d39571b223b59b2e55006683ba1e9feaabcc
sspadaro <spadaros@email.sc.edu>
2024-09-14 12:33:42 -0400

    question class

--- state/Question.java
+++ state/Question.java
@@ -5,15 +5,28 @@ public class Question {
     private int answer;
 
     public Question(int num1, int num2, Operation operation){
-
+        question = num1+ " "+operation+" "+num2+":";
+        if(operation.label.equals("+")){
+            answer = num1 + num2;
+        }else if(operation.label.equals("-")){
+            answer = num1 - num2;
+        }else if(operation.label.equals("*")){
+            answer = num1 * num2;
+        }else{
+            answer = num1/num2;
+        }
     }
     public String toString(){
-
+        return question;
     }
     public int getAnswer(){
-
+        return answer;
     }
     public boolean isCorrect(int answer){
-        
+        if(this.answer == answer){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
