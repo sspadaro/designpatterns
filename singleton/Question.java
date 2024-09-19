44c9aa3231fa489b39dd9234505cec01b369c61b
sspadaro <spadaros@email.sc.edu>
2024-09-19 16:55:49 -0400

    set up methods

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,5 +1,22 @@
 package singleton;
 
+import java.util.ArrayList;
+
 public class Question {
-    
+    private String question;
+    private ArrayList<String> answers;
+    private int correctAnswer;
+
+    public Question(String question, int correctAnswer, String answers){
+
+    }
+    public String toString(){
+
+    }
+    public boolean isCorrect(int userAnswer){
+
+    }
+    public String getCorrectAnswer(){
+        
+    }
 }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -1,5 +1,34 @@
 package singleton;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 public class TriviaGame {
-    
+    private static TriviaGame triviaGame;
+    private int score;
+    private int numGames;
+    private Random rand;
+    private ArrayList<Question> questions;
+
+    private TriviaGame(){
+
+    }
+    public static TriviaGame getInstance(){
+
+    }
+    public Question getQuestion(){
+
+    }
+    public boolean hasMoreQuestions(){
+
+    }
+    public void winRound(){
+
+    }
+    public void loseRound(){
+
+    }
+    public String getScores(){
+        
+    }
 }
