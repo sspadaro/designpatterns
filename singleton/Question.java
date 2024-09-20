2e18d9e6431532103cf4eb35c023c24d3c808ac8
sspadaro <spadaros@email.sc.edu>
2024-09-20 15:56:08 -0400

    java doc

--- singleton/Question.java
+++ singleton/Question.java
@@ -3,25 +3,47 @@ package singleton;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Creates a question, which has a question, array list of possible answers, and a correct answer
+ * @author Sophia Spadaro
+ */
 public class Question {
     private String question;
     private ArrayList<String> answers;
     private int correctAnswer;
 
+    /**
+     * Creates a question, which has a question, array list of possible answers, and a correct answer
+     * @param question the question being asked
+     * @param correctAnswer the index of the correct answer
+     * @param answers an array list of possible answers
+     */
     public Question(String question, int correctAnswer, String... answers){
         this.question = question;
         this.correctAnswer = correctAnswer;
         this.answers = new ArrayList<>(Arrays.asList(answers));
     }
+    /**
+     * Returns how the question and answers should be printed out
+     */
     public String toString(){
         return "\n"+question+"\n"+"1. "+answers.get(0)+"\n"
         +"2. "+answers.get(1)+"\n"
         +"3. "+answers.get(2)+"\n"
         +"4. "+answers.get(3)+"\n";
     }
+    /**
+     * Checks if the user's answer matches the correct answer
+     * @param userAnswer the user's answer
+     * @return true if the user was correct and false if the user was incorrect
+     */
     public boolean isCorrect(int userAnswer){
         return userAnswer == correctAnswer+1;
     }
+    /**
+     * Return the correct answer
+     * @return the correct answer
+     */
     public String getCorrectAnswer(){
         return answers.get(correctAnswer);
     }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -3,25 +3,39 @@ package singleton;
 import java.util.Random;
 import java.util.ArrayList;
 
+/**
+ * Creates a new trivia game
+ * @author Sophia Spadaro
+ */
 public class TriviaGame {
     private static TriviaGame triviaGame;
     private int score;
     private int numGames;
     private Random rand;
     private ArrayList<Question> questions;
-
+    /**
+     * Creates a trivia game using an array list of questions from the data loader. The singleton.
+     */
     private TriviaGame(){
         score = 0;
         numGames = 0;
         rand = new Random();
         questions = DataLoader.getTriviaQuestions();
     }
+    /**
+     * return an instance of trivia game
+     * @return an instane of trivia game
+     */
     public static TriviaGame getInstance(){
         if (triviaGame == null) {
 			triviaGame = new TriviaGame();
 		}
 		return triviaGame;
     }
+    /**
+     * Return a random question and remove it from the array list
+     * @return a random question
+     */
     public Question getQuestion(){
         if(hasMoreQuestions()){
             return questions.remove(rand.nextInt(questions.size()));
@@ -29,6 +43,10 @@ public class TriviaGame {
             return null;
         }
     }
+    /**
+     * Check if the array list has any more questions
+     * @return true if there are questions left and false if the list of questions is empty
+     */
     public boolean hasMoreQuestions(){
         if(questions.isEmpty()){
             return false;
@@ -36,13 +54,23 @@ public class TriviaGame {
             return true;
         }
     }
+    /**
+     * Increase the user's score and number of questions asked
+     */
     public void winRound(){
         score++;
         numGames++;
     }
+    /**
+     * Increase number of questions asked
+     */
     public void loseRound(){
         numGames++;
     }
+    /**
+     * Return number of questions the user got right out of number of questions answered
+     * @return the user's final score
+     */
     public String getScores(){
         return "You scored "+score+"/"+numGames;
     }
